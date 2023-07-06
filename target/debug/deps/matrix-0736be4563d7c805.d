b5c02f265911d2b43435d1a1bdb806e3fe41f7bb
hugorclt <hugorecolet@gmail.com>
2023-07-06 21:57:27 +0200

    [INIT] init lib

---
+++ target/debug/deps/matrix-0736be4563d7c805.d
@@ -0,0 +1,5 @@
+/home/hugo/Code/matrix/target/debug/deps/matrix-0736be4563d7c805.rmeta: src/lib.rs
+
+/home/hugo/Code/matrix/target/debug/deps/matrix-0736be4563d7c805.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/matrix-64ece4d48ca979f3.d
@@ -0,0 +1,5 @@
+/home/hugo/Code/matrix/target/debug/deps/matrix-64ece4d48ca979f3: src/lib.rs
+
+/home/hugo/Code/matrix/target/debug/deps/matrix-64ece4d48ca979f3.d: src/lib.rs
+
+src/lib.rs:
